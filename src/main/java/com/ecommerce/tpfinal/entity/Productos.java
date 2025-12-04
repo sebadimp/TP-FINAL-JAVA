8460f6ce3797a87382d40d8a2eef5223763dd5d9
sebadimp <seba.dimperio@gmail.com>
2025-12-04 12:43:52 -0300

    Ultimos ajustes!

--- src/main/java/com/ecommerce/tpfinal/entity/Productos.java
+++ src/main/java/com/ecommerce/tpfinal/entity/Productos.java
@@ -15,7 +15,6 @@ public class Productos {
     @GeneratedValue(strategy= GenerationType.IDENTITY)
 
     private Long id;
-
     private String nombre;
     private String descripcion;
     private Double precio;
